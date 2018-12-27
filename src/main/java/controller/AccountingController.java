a193bf6eadb5d4bc82cb38fade0e9e3ea1b5778a
Marcelo Gumiero <mgumiero9@gmail.com>
2018-12-27 20:24:14 -0200

    Accounting controller

--- src/main/java/controller/AccountingController.java
+++ src/main/java/controller/AccountingController.java
@@ -1,9 +1,16 @@
 package controller;
 
+import model.Accounting;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
 
 @RestController
 public class AccountingController {
 
-
+    @RequestMapping("/get-accounting")
+    public Accounting accountingGetFirst() {
+        return new Accounting(1, 1, new BigDecimal(1000));
+    }
 }
