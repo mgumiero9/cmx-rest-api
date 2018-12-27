54e1cb73e57fdde23174920b67a3bbf0815482fe
Marcelo Gumiero <mgumiero9@gmail.com>
2018-12-27 20:37:58 -0200

    Application fix.

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,3 +1,6 @@
+package application;
+
+import controller.AccountingController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -6,5 +9,6 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        new AccountingController().accountingGetFirst();
     }
 }
