48a55b20580c43741087c0f596166b9052d8ccd0
Marcelo Gumiero <mgumiero9@gmail.com>
2018-12-28 11:22:57 -0200

    Removed unused line.

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,6 +1,5 @@
 package application;
 
-import application.controller.AccountingController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +8,5 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        new AccountingController().accountingGetFirst();
     }
 }
