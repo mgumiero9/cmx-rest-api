1caaa8386a146fa90850f75a16e6c7d776f6cec4
Marcelo Gumiero <mgumiero9@gmail.com>
2018-12-28 13:25:13 -0200

    First simple post.

--- src/main/java/application/controller/AccountingController.java
+++ src/main/java/application/controller/AccountingController.java
@@ -2,8 +2,11 @@ package application.controller;
 
 
 import application.model.Accounting;
+import application.model.IMDatabase;
 import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
@@ -14,16 +17,21 @@ public class AccountingController implements ErrorController {
     private final static String GET_ACCOUNTING_PATH = "/get-accounting";
     private final static String ERRORPATH = "/error";
 
-    @RequestMapping(GET_ACCOUNTING_PATH)
-    public Accounting accountingGetFirst() {
+    @GetMapping(GET_ACCOUNTING_PATH)
+    private Accounting GetAccountingFirst() {
         System.out.println("test");
         return new Accounting(1, 1, new BigDecimal(1000));
     }
 
     @Override
-    @RequestMapping(ERRORPATH)
+    @GetMapping(ERRORPATH)
     public String getErrorPath() {
         return "No Mapping Found";
+    }
+
+    @PostMapping("/lancamentos-contabeis")
+    private Accounting newAccountEntry(@RequestBody Accounting newAccountEntry) {
+        return IMDatabase.getInstance().save(newAccountEntry);
     }
 
 }
