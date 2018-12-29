74725727225f3b18f2664a5add80110a6556efcb
Marcelo Gumiero <mgumiero9@gmail.com>
2018-12-29 12:03:42 -0200

    validations.

--- src/main/java/application/model/Accounting.java
+++ src/main/java/application/model/Accounting.java
@@ -1,12 +1,19 @@
 package application.model;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 public class Accounting {
 
     private String id;
+    @NotNull
     private Integer contaContabil;
+    @Digits(integer = 8, fraction = 0)
     private Integer data;
+    @Min(0)
+    @Digits(integer = 20, fraction = 2)
     private BigDecimal valor;
 
     public Accounting(Integer contaContabil, Integer data, BigDecimal valor) {
