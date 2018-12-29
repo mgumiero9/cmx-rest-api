690e32a0f57a26367a7547fc203caa2120dd90ad
Marcelo Gumiero <mgumiero9@gmail.com>
2018-12-29 12:05:04 -0200

    genericExceptionHandlers working

--- src/main/java/application/controller/AccountingController.java
+++ src/main/java/application/controller/AccountingController.java
@@ -8,11 +8,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
@@ -33,11 +35,14 @@ public class AccountingController implements ErrorController {
     }
 
     @PostMapping("/lancamentos-contabeis")
-    private ResponseEntity<JsonNode> newAccountEntry(@RequestBody Accounting newAccountEntry) {
+    private ResponseEntity<JsonNode> newAccountEntry(@Valid @RequestBody Accounting newAccountEntry, Errors errors) {
+
         ObjectMapper objectMapper = new ObjectMapper();
         Map<String, String> responseJson = new HashMap<>();
+
         responseJson.put("ID", IMDatabase.getInstance().save(newAccountEntry));
         JsonNode jsonNode = objectMapper.valueToTree(responseJson);
+
         return new ResponseEntity<>(jsonNode, HttpStatus.CREATED);
     }
 
