b11049dff4befe18993ae245c73f146cf2599e4d
Marcelo Gumiero <mgumiero9@gmail.com>
2019-01-06 13:43:26 -0200

    Fix Swagger generator configuration.

--- src/main/java/application/config/SwaggerConfig.java
+++ src/main/java/application/config/SwaggerConfig.java
@@ -19,7 +19,7 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
     public Docket greetingApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("application.config"))
+                .apis(RequestHandlerSelectors.basePackage("application.controller"))
                 .build()
                 .apiInfo(metaData());
 
